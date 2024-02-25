ce2f2e2077b5dfeb98d9c22858531285528b775e
victorcho@outlook.com <victorcho@outlook.com>
2024-02-25 12:29:11 -0800

    Init Push

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM maven:3.9-sapmachine-21 AS build
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM openjdk:21-jdk-slim
+COPY --from=build /target/assignment2-0.0.1-SNAPSHOT.jar assignment2.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "assignment2.jar"]
---
+++ src/main/java/group9/sfursmeetingapplication/SfursmeetingapplicationApplication.java
@@ -0,0 +1,13 @@
+package group9.sfursmeetingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfursmeetingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfursmeetingapplicationApplication.class, args);
+	}
+
+}
