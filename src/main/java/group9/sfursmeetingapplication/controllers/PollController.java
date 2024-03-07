c387e3c3bfc425ccb8d6795646f9757c96323c04
Shawn Oborne <shawn_oborne@sfu.ca>
2024-03-06 16:08:19 -0800

    added support for detecting user info on backend, once sessions are set up

--- src/main/java/group9/sfursmeetingapplication/controllers/PollController.java
+++ src/main/java/group9/sfursmeetingapplication/controllers/PollController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import group9.sfursmeetingapplication.models.Poll;
 import group9.sfursmeetingapplication.models.PollRepository;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PollController {
@@ -16,15 +17,25 @@ public class PollController {
     @Autowired
     private PollRepository pollRepo;
     @GetMapping(value = "/dashboard")
-    public String getAllStudents(Model model){
+    public String getAllStudents(Model model, HttpSession session){
+        // User user = (User) session.getAttribute("user");
+        // if (user == null){
+        //     //not logged in, redirect
+        //     return "login";
+        // }
+        // else {
 
-        //get from DB
-        //get all polls this user has been invited to
-        //  could in the future move results the user has answered
-        List<Poll> polls = pollRepo.findByUID(1);
-        
-        model.addAttribute("polls", polls);
-        
-        return "userDashboard";
+            //generate dashboard
+
+            //get from DB
+            //get all polls this user has been invited to
+            //  could in the future move results the user has answered
+            List<Poll> polls = pollRepo.findByUID(user.uid);
+            
+            model.addAttribute("polls", polls);
+            return "userDashboard";
+
+            
+        //}
     }
 }
