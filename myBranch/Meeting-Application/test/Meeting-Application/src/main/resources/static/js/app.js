100ddbce633d7311a32c795db1da2ef397cfcfa6
spuriNV <136392914+spuriNV@users.noreply.github.com>
2024-03-11 22:52:46 -0700

    Delete myBranch/Meeting-Application/test/Meeting-Application directory

--- myBranch/Meeting-Application/test/Meeting-Application/src/main/resources/static/js/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * This function is called to all pages where the user is notified
- * of a successful operation or a failed operation.
- */
-function redirectAfterDelay() {
-    setTimeout(function () {
-        window.location.href = '/';
-    }, 3000);
-}
