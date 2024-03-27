86fe1763a5a8159f9f2d79671f2c7f0580f33c13
spuriNV <spoori2016@gmail.com>
2024-03-26 18:28:23 -0700

    organizer, medium, and invited unit tests

---
+++ src/test/java/group9/sfursmeetingapplication/modelTests/InvitedTest.java
@@ -0,0 +1,53 @@
+package group9.sfursmeetingapplication.modelTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import group9.sfursmeetingapplication.models.Invited;
+
+public class InvitedTest {
+    private Invited invited; 
+
+    @BeforeEach
+    void setUp() {
+        invited = new Invited(1, 2, 3);
+    }
+
+
+    @Test
+    void testGetPid() {
+        assertEquals(1, invited.getPid());
+    }
+
+    @Test
+    void testSetPid() {
+        invited.setPid(10);
+        assertEquals(10, invited.getPid());
+    }
+
+    @Test
+    void testGetUid() {
+        assertEquals(2, invited.getUid());
+    }
+
+    @Test
+    void testSetUid() {
+        invited.setPid(20);
+        assertEquals(20, invited.getUid());
+    }
+
+    @Test
+    void testGetIid() {
+        assertEquals(3, invited.getIid());
+    }
+
+    @Test
+    void testSetIid() {
+        invited.setPid(30);
+        assertEquals(30, invited.getIid());
+    }
+
+    
+}
---
+++ src/test/java/group9/sfursmeetingapplication/modelTests/MediumTest.java
@@ -0,0 +1,53 @@
+package group9.sfursmeetingapplication.modelTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import group9.sfursmeetingapplication.models.Medium;
+public class MediumTest {
+
+    private Medium medium; 
+
+    @BeforeEach
+    void setUp() {
+        medium = new Medium(1,"Burnaby", false);
+    }
+
+    @Test
+    void testGetMid() {
+        assertEquals(1, medium.getMid());
+    }
+
+    @Test
+    void testSetMid() {
+        medium.setMid(2);
+        assertEquals(2, medium.getMid());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Burnaby", medium.getName());
+    }
+
+    @Test
+    void testSetName() {
+        medium.setName("Surrey");
+        assertEquals("Surrey", medium.getName());
+    }
+
+    @Test
+    void testGetRemote() {
+        assertEquals(false, medium.getRemote());
+    }
+
+    @Test
+    void testSetRemote() {
+        medium.setRemote(true);
+        assertEquals(true, medium.getRemote());
+    }
+
+    
+    
+}
